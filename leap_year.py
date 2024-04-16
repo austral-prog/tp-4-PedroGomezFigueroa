1c7a0af7b2acad7a5024adf4d26c927099505bfc
PedroGomezFigueroa <162721991+PedroGomezFigueroa@users.noreply.github.com>
2024-04-16 12:52:50 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,16 @@
 def leap_year():
-    print("TO DO")
+    ano = int(input("Ingrese un año:"))
+    cuatro = ((float(ano/4))-(int(ano/4)))
+    cien = ((float(ano/100))-(int(ano/100)))
+    cuatrociento = ((float(ano/400))-(int(ano/400)))
+
+    if (cuatrociento==0):
+        print(f"El año {ano} es bisiesto")
+    elif(cien > 0) and (cuatro==0):
+        print(f"El año {ano} es bisiesto")
+    else:
+        print(f"El año {ano} no es bisiesto")
+
+
+
+
